419730ae5029046c5df2a9a86a57af371149aa03
Sloubi <git@sloubi.eu>
2015-09-10 21:41:39 +0200

    Fix mysql encoding in Install

--- Prolio/Controller/Install.php
+++ Prolio/Controller/Install.php
@@ -70,7 +70,10 @@ class Install
             $this->pdo = new \PDO('mysql:host='.$this->app->request->post('db_host').';dbname='.$this->app->request->post('db_name'),
                 $this->app->request->post('db_user'),
                 $this->app->request->post('db_passw'),
-                array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION)
+                array(
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+                )
             );
         }
         catch (\Exception $e)

a8c0d326563a35bc3b5c191db30c5556d647bf00
Sloubi <git@sloubi.eu>
2015-10-25 13:22:53 +0100

    Improvements in FrontController (index.php)

--- web/index.php
+++ web/index.php
@@ -5,32 +5,10 @@ ini_set("display_errors", 1);
 
 require '../vendor/autoload.php';
 
+DEFINE('PUBLIC_DIR', __DIR__);
+
 // Initialize Slim
 $app = new \Slim\Slim();
-
-// Configuration
-DEFINE('PUBLIC_DIR', dirname(__FILE__));
-$config = false;
-if (file_exists('../config/config.php'))
-{
-    require '../config/config.php';
-    $app->config($config);
-}
-
-// Session
-session_cache_limiter(false);
-session_start();
-
-// Debug
-$debugbar = new \Slim\Middleware\DebugBar();
-$app->add($debugbar);
-
-// Database
-if ($config)
-{
-    $dbConfig = $app->config('database');
-    \Prolio\Model\Spdo::setMysqlParams($dbConfig['host'], $dbConfig['name'], $dbConfig['user'], $dbConfig['pass']);
-}
 
 // View
 $app->view = new \Slim\Views\Twig();
@@ -41,23 +19,37 @@ $app->view->parserOptions = array(
 );
 $app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
 
-// Title
-if ($config)
-{
-    $pageModel = new \Prolio\Model\Page();
-    $home = $pageModel->getBySlug('home');
-    $app->view->getInstance()->addGlobal('siteTitle', $home->name);
-}
-
-// Routes
-if ($config)
-{
-    require '../config/routes.php';
-}
-else
+// Need install?
+if (!file_exists('../config/config.php'))
 {
     $app->get('/', '\Prolio\Controller\Install:index')->via('GET', 'POST')->name('install');
+    $app->run();
+    exit;
 }
+
+// Configuration
+require '../config/config.php';
+$app->config($config);
+
+// Session
+session_cache_limiter(false);
+session_start();
+
+// Debug
+$debugbar = new \Slim\Middleware\DebugBar();
+$app->add($debugbar);
+
+// Database
+$dbConfig = $app->config('database');
+\Prolio\Model\Spdo::setMysqlParams($dbConfig['host'], $dbConfig['name'], $dbConfig['user'], $dbConfig['pass']);
+
+// Site Title
+$pageModel = new \Prolio\Model\Page();
+$home = $pageModel->getBySlug('home');
+$app->view->getInstance()->addGlobal('siteTitle', $home->name);
+
+// Routes
+require '../config/routes.php';
 
 // Go
 $app->run();
